9329022e10cab5357dd25843155788caac7ae671
Fidele K.Cyisa <fihacker000@gmail.com>
2022-02-07 21:13:29 +0300

    add

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,8 +1,22 @@
+import React,{ useState } from 'react';
 
 
-const ExpenseFilter =()=>{
+const ExpenseFilter =(props)=>{
+
+    const dropdownChanged=(event)=>{
+        props.onFilter(event.target.value)
+    }
+
     return (
-        
+        <div>
+            <select selected={props.selected} onChange={dropdownChanged}>
+                <option value="2020">2019</option>
+                <option value="2020">2020</option>
+                <option value="2020">2021</option>
+                <option value="2020">2022</option>
+                
+            </select>
+        </div>
     );
 }
 
